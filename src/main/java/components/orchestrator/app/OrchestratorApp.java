6088321a5222cca6cd0ed590c13542d4e2ca4843
joas-barros <joas.barros14@hotmail.com>
2026-04-16 15:20:22 -0300

    feat: ports class

--- src/main/java/components/orchestrator/app/OrchestratorApp.java
+++ src/main/java/components/orchestrator/app/OrchestratorApp.java
@@ -1,9 +1,10 @@
 package components.orchestrator.app;
 
 import components.orchestrator.src.Orchestrator;
+import utils.Ports;
 
 public class OrchestratorApp {
     static void main(String[] args) {
-        new Orchestrator(8080);
+        new Orchestrator(Ports.ORCHESTRATOR);
     }
 }
---
+++ src/main/java/utils/Ports.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Ports {
+
+    public static final int ORCHESTRATOR = 8080;
+}
