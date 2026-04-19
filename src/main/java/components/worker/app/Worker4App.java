8c92e3a5b4c53bb99892400fc165a15ed6e70905
joas-barros <joas.barros14@hotmail.com>
2026-04-19 12:24:27 -0300

    feat: implement new worker processes

---
+++ src/main/java/components/worker/app/Worker4App.java
@@ -0,0 +1,14 @@
+package components.worker.app;
+
+import components.worker.src.Worker;
+import utils.Ports;
+
+public class Worker4App {
+    static void main(String[] args) {
+        new Worker("localhost",
+                Ports.ORCHESTRATOR,
+                "localhost",
+                Ports.DATASERVER,
+                2);
+    }
+}
