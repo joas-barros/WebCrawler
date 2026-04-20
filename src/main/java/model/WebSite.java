7765c623ea6723ce9f2bb895389404dff8b8da59
lauramoroni <moronilaura28@gmail.com>
2026-04-20 16:11:28 -0300

    feat: update Dataserver and worker to handle WebSite objects and improve response format

--- src/main/java/model/WebSite.java
+++ src/main/java/model/WebSite.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class WebSite {
+public class WebSite implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String url;
     private String contentHTML;
     private List<String> links;
